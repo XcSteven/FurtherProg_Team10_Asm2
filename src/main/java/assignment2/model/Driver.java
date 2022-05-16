6c1686d9e419bba68e3eef263d50f9a44c216fd4
XcSteven <stevenbui1809@gmail.com>
2022-05-16 18:37:54 +0700

    Update createdBy

--- src/main/java/assignment2/model/Driver.java
+++ src/main/java/assignment2/model/Driver.java
@@ -1,5 +1,7 @@
 package assignment2.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
@@ -15,6 +17,9 @@ public class Driver {
 	private long id;
 
 	@CreatedDate
+	@Column(name = "created_date")
+	@JsonFormat(pattern = "dd.MM.YYYY")
+	@CreationTimestamp
 	private ZonedDateTime dateCreated;
 
 	@Column
